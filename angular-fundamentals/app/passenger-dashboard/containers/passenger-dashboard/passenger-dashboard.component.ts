74c05e510709c828361d64f083a7852a288c7dad
Mario Jonckers <mario_jonckers@peopleware.be>
2018-03-23 15:46:05 +0100

    ngOnInit lifecycle hook

--- angular-fundamentals/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ angular-fundamentals/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
@@ -22,36 +22,40 @@ import { Passenger } from '../../models/passenger.interface';
     </ul>
   </div>`
 })
-export class PassengerDashboardComponent {
-  private passengers: Passenger[] = [{
-    id: 1,
-    fullName: 'Stephen',
-    checkedIn: true,
-    checkedInDate: 1490742000000,
-    children: null
-  }, {
-    id: 2,
-    fullName: 'Rose',
-    checkedIn: false,
-    checkedInDate: null,
-    children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
-  }, {
-    id: 3,
-    fullName: 'James',
-    checkedIn: true,
-    checkedInDate: 1491606000000,
-    children: null
-  }, {
-    id: 4,
-    fullName: 'Louise',
-    checkedIn: true,
-    checkedInDate: 1488412800000,
-    children: [{ name: 'Jessica', age: 1 }]
-  }, {
-    id: 5,
-    fullName: 'Tina',
-    checkedIn: false,
-    checkedInDate: null,
-    children: null
-  }]
+export class PassengerDashboardComponent implements OnInit {
+  private passengers: Passenger[];
+
+  ngOnInit(): void {
+    this.passengers = [{
+      id: 1,
+      fullName: 'Stephen',
+      checkedIn: true,
+      checkedInDate: 1490742000000,
+      children: null
+    }, {
+      id: 2,
+      fullName: 'Rose',
+      checkedIn: false,
+      checkedInDate: null,
+      children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
+    }, {
+      id: 3,
+      fullName: 'James',
+      checkedIn: true,
+      checkedInDate: 1491606000000,
+      children: null
+    }, {
+      id: 4,
+      fullName: 'Louise',
+      checkedIn: true,
+      checkedInDate: 1488412800000,
+      children: [{ name: 'Jessica', age: 1 }]
+    }, {
+      id: 5,
+      fullName: 'Tina',
+      checkedIn: false,
+      checkedInDate: null,
+      children: null
+    }]
+  }
 }
