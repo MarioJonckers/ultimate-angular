1d30d4954c71e03c9926ebfdc9c7af238321d07f
Mario Jonckers <mario_jonckers@peopleware.be>
2018-03-26 12:29:06 +0200

    Hash location strategy

--- angular-fundamentals/app/app.module.ts
+++ angular-fundamentals/app/app.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     // Angular Modules
     BrowserModule,
     CommonModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { useHash: true}),
     // Custom Modules
     PassengerDashboardModule
   ],
