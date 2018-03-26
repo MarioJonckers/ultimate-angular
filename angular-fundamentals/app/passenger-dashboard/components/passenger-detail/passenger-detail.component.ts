2b86b64cdc2122ecbbf83e355abec3effa49ef3e
Mario Jonckers <mario_jonckers@peopleware.be>
2018-03-26 11:34:35 +0200

    Remove unused imports

--- angular-fundamentals/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ angular-fundamentals/app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
--- angular-fundamentals/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ angular-fundamentals/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
 import { Baggage } from '../../models/bagage.interface';
-import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'passenger-form',
