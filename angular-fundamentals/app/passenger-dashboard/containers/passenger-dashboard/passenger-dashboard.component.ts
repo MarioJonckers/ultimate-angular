c9e224acba5ed4673168ccf2d4dae16c89bd8f63
Mario Jonckers <mario_jonckers@peopleware.be>
2018-03-26 09:59:12 +0200

    Observable.catch error handling

--- angular-fundamentals/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ angular-fundamentals/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -25,23 +25,26 @@ export class PassengerDashboardComponent implements OnInit {
   constructor(private passengerService: PassengerDashboardService) {}
 
   ngOnInit(): void {
-    this.passengerService.getPassengers().then((data: Passenger[]) => this.passengers = data);
+    this.passengerService.getPassengers()
+      .subscribe((data: Passenger[]) => this.passengers = data);
   }
 
   private handleEdit(event: Passenger): void {
-    this.passengerService.updatePassenger(event).then((data: Passenger) => {
-      this.passengers = this.passengers.map((passenger: Passenger) => {
-        if (passenger.id === event.id) {
-          passenger = Object.assign({}, passenger, event);
-        }
-        return passenger;
+    this.passengerService.updatePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passengers = this.passengers.map((passenger: Passenger) => {
+          if (passenger.id === event.id) {
+            passenger = Object.assign({}, passenger, event);
+          }
+          return passenger;
+        });
       });
-    });
   }
 
   private handleRemove(event: Passenger): void {
-    this.passengerService.removePassenger(event).then((data: Passenger) => {
-      this.passengers = this.passengers.filter((passenger: Passenger) => passenger.id !== event.id);
-    });
+    this.passengerService.removePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passengers = this.passengers.filter((passenger: Passenger) => passenger.id !== event.id);
+      });
   }
 }
