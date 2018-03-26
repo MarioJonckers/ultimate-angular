c558269441aec9646a543aa678469b7216ffb629
Mario Jonckers <mario_jonckers@peopleware.be>
2018-03-26 08:29:10 +0200

    Data Services and Dependency Injection

--- angular-fundamentals/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ angular-fundamentals/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
+import { PassengerDashboardService } from '../../passenger-dashboard.service';
 
 @Component({
   selector: 'passenger-dashboard',
@@ -21,38 +22,10 @@ import { Passenger } from '../../models/passenger.interface';
 export class PassengerDashboardComponent implements OnInit {
   private passengers: Passenger[];
 
+  constructor(private passengerService: PassengerDashboardService) {}
+
   ngOnInit(): void {
-    this.passengers = [{
-      id: 1,
-      fullName: 'Stephen',
-      checkedIn: true,
-      checkedInDate: 1490742000000,
-      children: null
-    }, {
-      id: 2,
-      fullName: 'Rose',
-      checkedIn: false,
-      checkedInDate: null,
-      children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
-    }, {
-      id: 3,
-      fullName: 'James',
-      checkedIn: true,
-      checkedInDate: 1491606000000,
-      children: null
-    }, {
-      id: 4,
-      fullName: 'Louise',
-      checkedIn: true,
-      checkedInDate: 1488412800000,
-      children: [{ name: 'Jessica', age: 1 }]
-    }, {
-      id: 5,
-      fullName: 'Tina',
-      checkedIn: false,
-      checkedInDate: null,
-      children: null
-    }]
+    this.passengers = this.passengerService.getPassengers();
   }
 
   private handleEdit(event: Passenger): void {
