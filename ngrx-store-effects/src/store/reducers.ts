ea89a1885d831dbcbfd4a56c93c66e33c44a26c9
Mario Jonckers <mario_jonckers@peopleware.be>
2018-04-10 13:19:44 +0200

    All about Reducers

---
+++ ngrx-store-effects/src/store/reducers.ts
@@ -0,0 +1,20 @@
+export const initialstate = {
+  loaded: false,
+  loadeding: false,
+  data: [{label: 'Eat pizza', complete: false}]
+};
+
+export function reducer(state = initialstate, action: { type: string; payload: any }) {
+  switch (action.type) {
+    case 'ADD_TODO': {
+      const todo = action.payload;
+      const data = [...state.data, todo];
+      return {
+        ...state,
+        data
+      }
+    }
+  }
+
+  return state;
+}
